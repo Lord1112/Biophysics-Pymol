4a865cc9a85ea2c0c42ee5eb7f2a5da07241037a
Yuskiii <adria.navarropau@gmail.com>
2023-11-09 10:49:26 +0100

    ejercicio 1 python

---
+++ step1.py
@@ -0,0 +1,28 @@
+from Bio.PDB import PDBParser
+
+def get_interface_residues(structure, chain, distance_threshold):
+    interface_residues = set()
+
+    for model in structure:
+        for residue_a in model[chain]:
+            for residue_e in model[chain]:
+                if residue_a != residue_e and residue_a - residue_e < distance_threshold:
+                    interface_residues.add(residue_a)
+                    interface_residues.add(residue_e)
+
+    return list(interface_residues)
+
+# Cambia "tu_estructura.pdb" al nombre de tu archivo PDB y ajusta "A" y "B" a las cadenas relevantes.
+parser = PDBParser(QUIET=True)
+structure = parser.get_structure("6m0j.pdb", "6mj0.pdb")
+
+#distancia de contacto que determinaste en PyMOL
+distance_threshold = 2.0
+
+# Obtenemos los residuos de la interfaz para cada cadena
+interface_residues_chain_A = get_interface_residues(structure, "A", distance_threshold)
+interface_residues_chain_E = get_interface_residues(structure, "E", distance_threshold)
+
+# Imprimimos los resultados
+print(f"Residuos de la interfaz en la cadena A: {interface_residues_chain_A}")
+print(f"Residuos de la interfaz en la cadena E: {interface_residues_chain_E}")
