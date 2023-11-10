fca0e1bff491c8224eb72fe62535ad104c4b535e
IsGlak <ismael.romero@alum.esci.upf.edu>
2023-11-10 09:56:43 +0100

    change to script

--- scripts.py
+++ scripts.py
@@ -31,17 +31,17 @@ clStructure.add(clModel)
 #Save the cleaned structure to a pdb file
 pdbCreator=PDBIO()
 pdbCreator.set_structure(clStructure)
-pdbCreator.save("cl_d6m0j.pdb")
+#dbCreator.save("cl_d6m0j.pdb")
 
 import biobb_structure_checking
 import biobb_structure_checking.constants as cts
 from biobb_structure_checking.structure_checking import StructureChecking
 base_dir_path=biobb_structure_checking.__path__[0]
-base_path = ''
+
 args = cts.set_defaults(base_dir_path,{'notebook':True})
-args['input_structure_path'] = base_path + '6m0j.cif'
-args['output_structure_path'] = base_path + '6m0j_fixed.pdb'
-args['output_structure_path_charges'] = base_path + '6m0j_fixed.pdbqt'
+args['input_structure_path'] = 'cl_6m0j.pdb'
+args['output_structure_path'] = 'cl_6m0j_fixed.pdb'
+args['output_structure_path_charges'] = 'cl_6m0j_fixed.pdbqt'
 args['debug'] = False
 args['verbose'] = False
 #Added missing defaults
@@ -52,3 +52,8 @@ args['build_warnings'] = False
 args['coords_only'] = False
 args['overwrite'] = False
 args['output_format'] = 'pdb'
+
+st_c = StructureChecking(base_dir_path, args)
+
+st_c.add_hydrogen()
+st_c._save_structure("hola.pdb")
