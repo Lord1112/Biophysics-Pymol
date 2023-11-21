f883eb5288f5f35b15e6833091bafe7bdd3ec025
IsGlak <ismael.romero@alum.esci.upf.edu>
2023-11-21 16:01:39 +0100

    preparation again

---
+++ preparation.py
@@ -0,0 +1,65 @@
+from Bio.PDB import *
+from Bio.PDB import Chain
+from Bio.PDB import Model
+from Bio.PDB import Structure
+from Bio.PDB.PDBIO import PDBIO
+def chainCleaner(chain,chainID):
+    cleanedChain = Chain.Chain(chainID)
+    for i in chain.get_residues():
+        if i.resname in {'ALA', 'CYS', 'ASP', 'GLU', 'PHE', 'GLY', 'HIS', 'ILE', 'LYS', 'LEU', 'MET', 'ASN', 'PRO', 'GLN', 'ARG', 'SER', 'THR', 'VAL', 'TRP', 'TYR'}:
+            cleanedChain.add(i)
+    return(cleanedChain)
+
+def chainCleanerDetach(structure,chain_id):
+    for i in structure[0][chain_id]:
+        if i.resname not in {'ALA', 'CYS', 'ASP', 'GLU', 'PHE', 
+                             'GLY', 'HIS', 'ILE', 'LYS', 'LEU', 
+                             'MET', 'ASN', 'PRO', 'GLN', 'ARG', 
+                             'SER', 'THR', 'VAL', 'TRP', 'TYR'}:
+            print(structure[0][chain_id].__getitem__(i.id)["het"])
+            structure[0][chain_id].detach_child(i.id)
+
+parser = PDBParser(get_header=True,QUIET=True)
+structure=parser.get_structure("6m0j","6m0j.pdb")
+chainCleanerDetach(structure,"A")
+chainCleanerDetach(structure,"E")
+
+pdbCreator=PDBIO()
+
+pdbCreator.set_structure(structure)
+pdbCreator.save("cl_d6m0j.pdb")
+
+
+
+parser = PDBParser(QUIET=True)
+unStructure=parser.get_structure("6m0j","6m0j.pdb")
+unChainA = unStructure[0]["A"]
+unChainE = unStructure[0]["E"]
+unChainA = structure[0]["A"]
+unChainE = structure[0]["E"]
+
+clChainA= chainCleaner(unChainA,"A")
+clChainE= chainCleaner(unChainE,"E")
+import biobb_structure_checking.constants as cts
+from biobb_structure_checking.structure_checking import StructureChecking
+base_dir_path=biobb_structure_checking.__path__[0]
+base_path = ''
+
+args = cts.set_defaults(base_dir_path,{'notebook':True})
+args['input_structure_path'] = base_path + '6m0j.cif'
+args['output_structure_path'] = base_path + '6m0j_fixed.pdb'
+args['output_structure_path_charges'] = base_path + '6m0j_fixed.pdbqt'
+args['input_structure_path'] = 'cl_d6m0j.pdb'
+args['output_structure_path'] = 'cl_dm0j_fixed.pdb'
+args['output_structure_path_charges'] = 'cl_d6m0j_fixed.pdbqt'
+args['debug'] = False
+args['verbose'] = False
+#Added missing defaults
+args['overwrite'] = False
+args['output_format'] = 'pdb'
+args['keep_canonical'] = False
+
+st_c = StructureChecking(base_dir_path, args)
+
+st_c.add_hydrogen('auto')
+st_c._save_structure("fixed_6m0j.pdb")
